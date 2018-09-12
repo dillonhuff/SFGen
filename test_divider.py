1e117c1e99dea589e47f612edb83bee39f6006d6
Dillon Huff <dillonhuff@gmail.com>
2018-09-11 18:23:28 -0700

    NR does not work on one case I can find, 21 / 3?

--- test_divider.py
+++ test_divider.py
@@ -18,4 +18,18 @@ def test_21_divided_by_3():
     res = newton_raphson_divide(bv_from_int(width, 21), bv_from_int(width, 3))
     print('res =', res)
     assert(res == bv_from_int(width, 7))
+
+def test_14_divided_by_6():
+    width = 16
+    res = newton_raphson_divide(bv_from_int(width, 14), bv_from_int(width, 6))
+    print('res =', res)
+    assert(res == bv_from_int(width, 2))
+    
+
+def test_249_divided_by_18():
+    width = 16
+    res = newton_raphson_divide(bv_from_int(width, 249), bv_from_int(width, 18))
+    print('res =', res)
+    assert(res == bv_from_int(width, 249 // 18))
+    
     
