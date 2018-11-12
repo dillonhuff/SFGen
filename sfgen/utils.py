f99b267dd1d4bf7426be6a91c69cce86b2106af2
Dillon Huff <dillonhuff@gmail.com>
2018-11-11 16:27:21 -0800

    Moved verilog testbenches

--- sfgen/utils.py
+++ sfgen/utils.py
@@ -28,7 +28,7 @@ def run_cmd(cmd):
 
 def run_iverilog_test(mod_name):
     # Compile iverilogmod
-    assert(run_cmd('iverilog -o {0} {0}.v {0}_tb.v'.format(mod_name)))
+    assert(run_cmd('iverilog -o {0} {0}.v ./verilog_tbs/{0}_tb.v'.format(mod_name)))
     assert(run_cmd('./{0} > {0}_res.txt'.format(mod_name)))
 
     f = open('{0}_res.txt'.format(mod_name), 'r')
